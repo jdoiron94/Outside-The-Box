b0d97f15d46d45e9f8cb03f09e4c636b9b024756
Jacob <jdoiron@smcm.edu>
2015-09-19 18:08:58 -0400

    Learning
    Added a few sprites to test functionality, centered player on screen,
    locked player to the map's bounds

--- Game1.cs
+++ Game1.cs
@@ -2,18 +2,28 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace KineticCamp
-{
+using System;
+
+namespace KineticCamp {
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
-    {
+    public class Game1 : Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
-        {
+        Texture2D player;
+        Texture2D npc;
+        Texture2D map;
+        
+        Rectangle playerBounds;
+
+        int x;
+        int y;
+        int centX;
+        int centY;
+
+        public Game1() {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
@@ -24,8 +34,7 @@ namespace KineticCamp
         /// related content.  Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
         /// </summary>
-        protected override void Initialize()
-        {
+        protected override void Initialize() {
             // TODO: Add your initialization logic here
 
             base.Initialize();
@@ -35,10 +44,15 @@ namespace KineticCamp
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
-        protected override void LoadContent()
-        {
+        protected override void LoadContent() {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            player = Content.Load<Texture2D>("player");
+            npc = Content.Load<Texture2D>("npc");
+            map = Content.Load<Texture2D>("map");
+            centX = (Window.ClientBounds.Width - player.Width) / 2;
+            centY = (Window.ClientBounds.Height - player.Height) / 2;
+            playerBounds = new Rectangle(centX, centY, player.Width, player.Height);
 
             // TODO: use this.Content to load your game content here
         }
@@ -47,8 +61,7 @@ namespace KineticCamp
         /// UnloadContent will be called once per game and is the place to unload
         /// game-specific content.
         /// </summary>
-        protected override void UnloadContent()
-        {
+        protected override void UnloadContent() {
             // TODO: Unload any non ContentManager content here
         }
 
@@ -57,11 +70,29 @@ namespace KineticCamp
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        protected override void Update(GameTime gameTime) {
+            KeyboardState kbs = Keyboard.GetState();
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || kbs.IsKeyDown(Keys.Escape)) {
                 Exit();
-
+            }
+            if (kbs.IsKeyDown(Keys.W)) {
+                if (y + 2 < centY) {
+                    y += 2;
+                }
+            } else if (kbs.IsKeyDown(Keys.S)) {
+                if (y - 2 > -1 * (map.Height - centY - player.Height)) {
+                    y -= 2;
+                }
+            } else if (kbs.IsKeyDown(Keys.A)) {
+                if (x + 2 < centX) {
+                    x += 2;
+                }
+            } else if (kbs.IsKeyDown(Keys.D)) {
+                if (x - 2 > -1 * ((map.Width - centX - player.Width))) {
+                    x -= 2;
+                }
+            }
+            
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -71,11 +102,16 @@ namespace KineticCamp
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+           
+            spriteBatch.Begin();
+            spriteBatch.Draw(map, new Rectangle(x, y, map.Width, map.Height), Color.White);
+            spriteBatch.Draw(player, playerBounds, Color.White);
+            //spriteBatch.Draw(npc, new Rectangle(700, 700, npc.Width, npc.Height), Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
