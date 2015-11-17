48a9f97e4bf453f584fd0f8ca0639b7fcc4393f8
robert-beall <rbeall@smcm.edu>
2015-11-17 10:49:04 -0500

    Pit update pt. 1

---
+++ com/otb/api/wrapper/locatable/LavaPit.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OutsideTheBox.com.otb.api.wrapper.locatable
+{
+    public class LavaPit : Pit
+    {
+        private int damage; 
+        
+        public LavaPit(Texture2D texture, Vector2 location, int width, int height):
+            base(texture, location, width, height)
+        {
+            damage = 5; 
+        }
+
+        public int getDamage()
+        {
+            return damage; 
+        }
+
+        public override void update(InputManager inputManager)
+        {
+            inputManager.getPlayerManager().damagePlayer(damage);
+        }
+    }
+}
---
+++ com/otb/api/wrapper/locatable/Pit.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OutsideTheBox.com.otb.api.wrapper.locatable
+{
+    public class Pit : GameObject
+    {
+        private Rectangle size;
+
+        public Pit(Texture2D texture, Vector2 location, int width, int height):
+            base(texture, location)
+        {
+            size = new Rectangle((int) getLocation().X, (int) getLocation().Y, width, height); 
+        }
+
+        public virtual void update(InputManager inputManager)
+        {
+
+        }
+
+        public void draw(SpriteBatch batch)
+        {
+            batch.Draw(getTexture(), size, Color.White);
+        }
+    }
+}
