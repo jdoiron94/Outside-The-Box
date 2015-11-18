a55a3a31458745719a998f37cab7382f9efa0e35
robert-beall <rbeall@smcm.edu>
2015-11-18 16:17:30 -0500

    Two Input Barrier

---
+++ com/otb/api/wrapper/locatable/TwoInputBarrier.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OutsideTheBox.com.otb.api.wrapper.locatable
+{
+    public class TwoInputBarrier : Barrier
+    {
+        private PressButton input_1;
+        private PressButton input_2;
+
+        public TwoInputBarrier(Texture2D[] Textures, Vector2 Location, PressButton input_1, PressButton input_2):
+            base(Textures, Location)
+        {
+            this.input_1 = input_1;
+            this.input_2 = input_2; 
+        }
+
+        public void update()
+        {
+            if(input_1.isPushed() && input_2.isPushed())
+            {
+                setState(true);
+            }
+            else
+            {
+                setState(false);
+            }
+        }
+    }
+}
