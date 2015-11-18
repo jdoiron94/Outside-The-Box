0012d0bd115efde66e872f14f90b0c7d41f95d4d
robert-beall <rbeall@smcm.edu>
2015-11-18 16:00:19 -0500

    Acid Pit added

---
+++ com/otb/api/wrapper/locatable/AcidPit.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OutsideTheBox.com.otb.api.wrapper.locatable
+{
+    public class AcidPit : Pit
+    {
+        private int damage;
+
+        public AcidPit(Texture2D texture, Vector2 location, int width, int height) :
+            base(texture, location, width, height)
+        {
+            damage = 2;
+        }
+
+        public int getDamage()
+        {
+            return damage;
+        }
+
+        public override void update(InputManager inputManager)
+        {
+            inputManager.getPlayerManager().depleteMana(damage);
+        }
+    }
+}
+
